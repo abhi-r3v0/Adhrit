9d922ab45ef5e3ff4c008eadfb55270b4ca0347e
abhi-r3v0 <jmabhishek4@gmail.com>
2016-09-23 18:47:40 +0530

    Strings output into a file

--- APKRecon.py
+++ APKRecon.py
@@ -1,6 +1,8 @@
 import zipfile
 import os
 import sys
+import subprocess
+
 
 #To know the contents of a package
 def apkInfo(apk_name) :
@@ -20,7 +22,12 @@ def apkInfo(apk_name) :
     print "\n\n"
     if os.path.exists('Extracts') :
         os.chdir('Extracts')
-    dexstrings = os.system("strings classes.dex")
+
+    print "\n********************************************************************"
+    print "\n\t\t Executing Strings on classes.dex "
+    dexstrings = os.system('strings classes.dex > Strings.txt')
+    print "\n\n Output written to 'Strings.txt' found in the Extracts directory\n\n"
+
 
 #Main fuction starts here
 if __name__ == "__main__" :
