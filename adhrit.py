fe21f5ca0cb60d5ab33d6155bd5ba62e787d1f6d
abhi-r3v0 <jmabhishek4@gmail.com>
2017-01-03 01:04:07 +0530

    Analysis dump

--- adhrit.py
+++ adhrit.py
@@ -37,18 +37,37 @@ class Adhrit :
     #Know all about the application.
     def apkRip( self, apk_name ) :
 	
+	apk = zipfile.ZipFile(apk_name, 'r')
+	os.system("mkdir -p Analysis")
+
+	print "********************************************************************"
 	print "\n"
+        print "\n\t [+]  File input  :  " +apk.filename
+        print "\n\n"	
+
+	print "\n"
+	print "**********************  Package   ***************************"
+	print "\n"
+	labelCmd = "./aapt dump badging " + apk_name + "| grep package:\ name"
+	os.system(labelCmd)
+	print "\n\n"
+
 	print "**********************  Permissions  ***************************"
 	print "\n"
-	permCmd = "./aapt dump permissions "+apk_name	
+	permCmd = "./aapt dump permissions "+apk_name + " | tee Permissions.txt"	
     	os.system(permCmd)
 	print "\n\n"
 
-	#print "*******************  Obfuscation details  **********************"
-	#print "\n"
-	#confCmd = "./aapt"+apk_name
-	#os.system(confCmd)
+	print "************************  Manifest  *****************************"
+	print "\n"
+	confCmd = "./aapt dump badging "+apk_name+ " | tee Manifest.txt" 
+	os.system(confCmd)
+	print "\n"
+	os.system("mv Manifest.txt Analysis && mv Permissions.txt Analysis")
+	print "\n"
+	print " \t + + + + +   Manifest and permissions dump can be found in the 'Analysis' directory   + + + + +"
 	print "\n\n"    
+
 
 
 
@@ -58,15 +77,6 @@ class Adhrit :
     def apkInfo(self, apk_name) :
 
         apk = zipfile.ZipFile(apk_name, 'r')
-
-        print "********************************************************************"
-	print "\n"
-        print "\n\t [+]  Package :  " +apk.filename
-        print "\n\n"
-
-
-
-
 
         print "********************** Extracting Jar ***************************"
 	print "\n"
