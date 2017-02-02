5b87979137514c233d5388c5b27e0e830f6f843c
abhi-r3v0 <jmabhishek4@gmail.com>
2017-02-02 18:11:33 +0530

    Debug flagging partial

---
+++ Recons/APKSign.py
@@ -0,0 +1,37 @@
+import os
+import xml.etree.ElementTree as et
+
+def SignAPK (apk_name) :
+
+    if os.path.exists('Source-Smali') :
+        dnamesplit = apk_name.split('.')[0]
+        ddir = 'Source-Smali/'+dnamesplit+'/AndroidManifest.xml'
+        print "Smali source found"
+        if os.path.exists(ddir) :
+            print "Manifest found"
+            f = open(ddir, "a+")
+            line = f.readlines()
+            f.close()
+            xmlparse = et.parse(ddir)
+
+
+    else :
+        print "\n\n"
+        print "[!] Smali source not found. Extracting .."
+        snamesplit = apk_name.split('.')[0]
+        SmaliCmd = 'java -jar apktool.jar d -f '+apk_name
+        os.system(SmaliCmd)
+        if os.path.isdir(snamesplit) :
+            os.system('mkdir -p Source-Smali')
+            os.system('mv '+snamesplit+ ' Source-Smali')
+            print "\n[+]  Extraction complete. Smali code available in 'Source-Smali' Directory "
+
+        if os.path.exists('Source-Smali') :
+            dnamesplit = apk_name.split('.')[0]
+            ddir = '/'+dnamesplit+'/AndroidManifest.xml'
+            print "Smali source found"
+            Dir = 'Source-Smali/'+dnamesplit+'/'
+            for root, subdirList, files in os.walk(Dir):
+                for fname in files:
+                    if fname == 'AndroidManifest.xml' :
+                        print('\t[>] %s' % fname)
--- adhrit.py
+++ adhrit.py
@@ -7,8 +7,10 @@ import argparse
 import webbrowser
 from Recons.apkRecon import apkRip
 from Recons.apkExtract import apkInfo
+from Recons.StaticAnalyzer import CodeAnalyzer
 from Recons.VirusTotal import APICheck
 from Recons.SmaliExtract import Smali
+from Recons.APKSign import SignAPK
 
 
 
@@ -32,17 +34,20 @@ class Adhrit :
     def apkripper(self, apk_name) :
         apkRip(apk_name)
 
-
     def apkextractor(self, apk_name) :
         apkInfo(apk_name)
 
+    def codeanalyzer(self) :
+        CodeAnalyzer()
 
     def vtanalyzer(self, apk_name) :
         APICheck(apk_name)
 
-
     def smaliextractor(self, apk_name) :
         Smali(apk_name)
+
+    def apksigner(self, apk_name) :
+        SignAPK(apk_name)
 
 
 #Main fuction starts here
@@ -54,6 +59,8 @@ def main() :
     parser.add_argument("-r", help="Analyze APK without extraction")
     parser.add_argument("-x", help="Extract APK contents only")
     parser.add_argument("-s", help="Source code of the APK in Smali")
+    parser.add_argument("-d", help="Flag the APK debuggable and sign")
+    parser.add_argument("-l", help="Perform static analysis")
     parser.add_argument("-w", help="Welcome :P")
     parser.add_argument("-v", help="Check footprints in VirusTotal database")
     args = parser.parse_args()
@@ -62,8 +69,10 @@ def main() :
         adhrit.Welcome()
         adhrit.apkripper(args.a)
        	adhrit.apkextractor(args.a)
-        adhrit.smaliextractor(args.a)
         adhrit.vtanalyzer(args.a)
+        adhrit.smaliextractor(args.a)
+        adhrit.codeanalyzer()
+
 
     elif args.r :
         adhrit.Welcome()
@@ -78,6 +87,16 @@ def main() :
     elif args.s :
         adhrit.Welcome()
         adhrit.smaliextractor(args.s)
+
+
+    elif args.d :
+        adhrit.apksigner(args.d)
+
+
+    elif args.l :
+        adhrit.Welcome();
+        adhrit.apkextractor(args.l)
+        adhrit.codeanalyzer()
 
 
     elif args.w :
