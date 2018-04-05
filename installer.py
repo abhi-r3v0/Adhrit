d4ea07170cfd49171337f4bebfda3b9d27efc4b0
PS Narayanan <thepsnarayanan@gmail.com>
2018-04-05 22:22:18 +0530

    Installation fo MAC

--- installer.py
+++ installer.py
@@ -33,26 +33,26 @@ class dep_installer():
                 print "\n[!]  Error installing Android tools"
 
         elif platform == "darwin":
-            
+
                 print "\n[+]  Installing necessary tools on MAC"
             try:
-                os.system('sudo apt-get install toilet')
+                os.system('sudo brew install toilet')
                 print "\n[+]  Installation of dependencies complete"
             except OSError as ose:
                 print "\n[!]  Error installing dependency"
 
             print "\n[+]  Installing ARM dependencies"
             try:
-                os.system('sudo apt-get install libc6-armel-cross libc6-dev-armel-cross')
-                os.system('sudo apt-get install binutils-arm-linux-gnueabi')
-                os.system('sudo apt-get install libncurses5-dev')
+                os.system('sudo brew install libc6-armel-cross libc6-dev-armel-cross')
+                os.system('sudo brew install binutils-arm-linux-gnueabi')
+                os.system('sudo brew install libncurses5-dev')
                 print "\n[+]  Installation of ARM tools complete"
             except OSError as ose:
                 print "\n[!]  Error installing ARM dependencies"
 
             print "\n[+]  Installing Android debug tools "
             try:
-                os.system('sudo apt-get install android-tools-adb')
+                os.system('sudo brew install android-tools-adb')
                 print "\n[+]  Installation of Android tools complete"
             except OSError as ose:
                 print "\n[!]  Error installing Android tools"
