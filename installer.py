3f464cde6bd4d5b54864094f7f5a736e9498ebd6
PS Narayanan <thepsnarayanan@gmail.com>
2018-04-05 22:21:05 +0530

    Initial Code

--- installer.py
+++ installer.py
@@ -6,33 +6,59 @@ from sys import platform
 
 class dep_installer():
 
-    def __init__(self):
-        pass
-
     def ins(self):
-        print "\n[+]  Installing necessary tools "
-        try:
-            os.system('sudo apt-get install toilet')
-            print "\n[+]  Installation of dependencies complete"
-        except OSError as ose:
-            print "\n[!]  Error installing dependency"
+        if platform == "linux" or platform == "linux2":
+                # linux
+            print "\n[+]  Installing necessary tools on Linux"
+            try:
+                os.system('sudo apt-get install toilet')
+                print "\n[+]  Installation of dependencies complete"
+            except OSError as ose:
+                print "\n[!]  Error installing dependency"
 
-        print "\n[+]  Installing ARM dependencies"
-        try:
-            os.system('sudo apt-get install libc6-armel-cross libc6-dev-armel-cross')
-            os.system('sudo apt-get install binutils-arm-linux-gnueabi')
-            os.system('sudo apt-get install libncurses5-dev')
-            print "\n[+]  Installation of ARM tools complete"
-        except OSError as ose:
-            print "\n[!]  Error installing ARM dependencies"
+            print "\n[+]  Installing ARM dependencies"
+            try:
+                os.system('sudo apt-get install libc6-armel-cross libc6-dev-armel-cross')
+                os.system('sudo apt-get install binutils-arm-linux-gnueabi')
+                os.system('sudo apt-get install libncurses5-dev')
+                print "\n[+]  Installation of ARM tools complete"
+            except OSError as ose:
+                print "\n[!]  Error installing ARM dependencies"
 
-        print "\n[+]  Installing Android debug tools "
-        try:
-            os.system('sudo apt-get install android-tools-adb')
-            print "\n[+]  Installation of Android tools complete"
-        except OSError as ose:
-            print "\n[!]  Error installing Android tools"
+            print "\n[+]  Installing Android debug tools "
+            try:
+                os.system('sudo apt-get install android-tools-adb')
+                print "\n[+]  Installation of Android tools complete"
+            except OSError as ose:
+                print "\n[!]  Error installing Android tools"
 
+        elif platform == "darwin":
+            
+                print "\n[+]  Installing necessary tools on MAC"
+            try:
+                os.system('sudo apt-get install toilet')
+                print "\n[+]  Installation of dependencies complete"
+            except OSError as ose:
+                print "\n[!]  Error installing dependency"
+
+            print "\n[+]  Installing ARM dependencies"
+            try:
+                os.system('sudo apt-get install libc6-armel-cross libc6-dev-armel-cross')
+                os.system('sudo apt-get install binutils-arm-linux-gnueabi')
+                os.system('sudo apt-get install libncurses5-dev')
+                print "\n[+]  Installation of ARM tools complete"
+            except OSError as ose:
+                print "\n[!]  Error installing ARM dependencies"
+
+            print "\n[+]  Installing Android debug tools "
+            try:
+                os.system('sudo apt-get install android-tools-adb')
+                print "\n[+]  Installation of Android tools complete"
+            except OSError as ose:
+                print "\n[!]  Error installing Android tools"
+
+        elif platform == "win32":
+                #Windows installation here
 
 def main():
     dep = dep_installer()
