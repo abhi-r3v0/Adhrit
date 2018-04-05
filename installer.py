8a16db5a0daca6f73b5a7f890fdb5095ae03c9da
PS Narayanan <thepsnarayanan@gmail.com>
2018-04-05 22:31:47 +0530

    Removing sudo in Mac installation as sudo mode is not supported in homebrew

--- installer.py
+++ installer.py
@@ -36,23 +36,23 @@ class dep_installer():
 
             print "\n[+]  Installing necessary tools on MAC"
             try:
-                os.system('sudo brew install toilet')
+                os.system('brew install toilet')
                 print "\n[+]  Installation of dependencies complete"
             except OSError as ose:
                 print "\n[!]  Error installing dependency"
 
             print "\n[+]  Installing ARM dependencies"
             try:
-                os.system('sudo brew install libc6-armel-cross libc6-dev-armel-cross')
-                os.system('sudo brew install binutils-arm-linux-gnueabi')
-                os.system('sudo brew install libncurses5-dev')
+                os.system('brew install libc6-armel-cross libc6-dev-armel-cross')
+                os.system('brew install binutils-arm-linux-gnueabi')
+                os.system('brew install libncurses5-dev')
                 print "\n[+]  Installation of ARM tools complete"
             except OSError as ose:
                 print "\n[!]  Error installing ARM dependencies"
 
             print "\n[+]  Installing Android debug tools "
             try:
-                os.system('sudo brew install android-tools-adb')
+                os.system('brew install android-tools-adb')
                 print "\n[+]  Installation of Android tools complete"
             except OSError as ose:
                 print "\n[!]  Error installing Android tools"
