b7b2f89b8711107661f5240671f54550c42fbea6
PS Narayanan <thepsnarayanan@gmail.com>
2018-04-06 10:22:01 +0530

    Adding binutils for mac

--- installer.py
+++ installer.py
@@ -44,7 +44,7 @@ class dep_installer():
             print "\n[+]  Installing ARM dependencies"
             try:
                 os.system('brew install libc6-armel-cross libc6-dev-armel-cross')
-                os.system('brew install binutils-arm-linux-gnueabi')
+                os.system('brew install binutils')
                 os.system('brew install libncurses5-dev')
                 print "\n[+]  Installation of ARM tools complete"
             except OSError as ose:
