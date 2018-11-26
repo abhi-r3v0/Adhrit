f46fd1009fc6f781ea286e78c0fe47c5fbf9d231
th3-j0k3r <akhilmahendra7991@gmail.com>
2018-11-26 21:27:00 +0530

    Added root detection module

---
+++ recons/root.py
@@ -0,0 +1,35 @@
+# !/usr/bin/python
+
+import subprocess
+
+paths = ["/system/app/Superuser.apk",
+         "/system/priv-app/Superuser.apk",
+         "/system/priv-app/superuser.apk",
+         "/system/app/superuser.apk",
+         "/sbin/su",
+         "/system/bin/su",
+         "/system/xbin/su",
+         "/data/local/xbin/su",
+         "/data/local/bin/su",
+         "/system/sd/xbin/su",
+         "/system/bin/failsafe/su",
+         "/data/local/su",
+         "/su/bin/su"]
+
+def check_root():
+	print "\n--------------------------------------------------"
+	print "[+] CHECKING ROOT STATUS"
+	print "----------------------------------------------------"
+	device = subprocess.check_output('adb devices',shell=True)
+	if len(device) is not 26:
+		for path in paths:
+			cmd = "adb shell 'if [ -f "+ path +" ];then echo 1; else echo 0; fi'"
+			check_root = subprocess.check_output(cmd, shell=True)
+			if check_root is 1:
+				print "Device is rooted"
+				break;
+		print "Device is not rooted" 
+	else:
+		print "Device is not Connected"
+
+check_root()
