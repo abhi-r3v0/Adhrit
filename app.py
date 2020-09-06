93fa322a98115fe2b5d6bfd52d862d19c581b7b5
Rahul Sani <rahulsani@Rahuls-MacBook-Pro.local>
2020-09-07 00:51:31 +0530

    Updated Report API

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from adhrit.adhrit import main
-from flask import  Flask, render_template, request, jsonify
+from flask import Flask, json, jsonify, render_template, request
 from threading import Thread
 from http import HTTPStatus
 import os, configparser, time
 from adhrit.recons.dbaccess import dbconnection, select_query
 from adhrit.recons.reset import reset_scanid, reset_db
+import sqlite3
 
 
 ALLOWED_EXTENSIONS = {'apk'}
@@ -14,13 +15,13 @@ app = Flask(__name__)
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	return '.' in filename and \
+		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def get_config_data(key):
-    check_deps = configparser.ConfigParser()
-    check_deps.read('adhrit/config')                                         ####
-    return check_deps.get('config-data', str(key))
+	check_deps = configparser.ConfigParser()
+	check_deps.read('adhrit/config')                                         ####
+	return check_deps.get('config-data', str(key))
 
 def update_scanid():
 
@@ -39,51 +40,78 @@ def update_scanid():
 
 @app.route('/scan',  methods=['POST'])
 def scan():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return HTTPStatus.NO_CONTENT
+	if request.method == 'POST':
+		if 'file' not in request.files:
+			flash('No file part')
+			return HTTPStatus.NO_CONTENT
 
-        uploaded_files = request.files["file"]
-        if uploaded_files and allowed_file(uploaded_files.filename):
-            uploaded_files.save(uploaded_files.filename)
-            # Renaming to app.apk
-            source =  uploaded_files.filename
-            dest = 'app.apk'
-            os.rename(source, dest)
-            
-          
-            main()
-            thesid = get_config_data('scan_id')
-            rows = getreport(thesid)
-            response = jsonify(status_msg='Scanning Completed', scan_id = thesid,rows = rows,  status=HTTPStatus.OK)
-            update_scanid()
-            return  response,{'Access-Control-Allow-Origin': '*'}
-    return jsonify(status_msg="apk not sent properly")
+		uploaded_files = request.files["file"]
+		if uploaded_files and allowed_file(uploaded_files.filename):
+			uploaded_files.save(uploaded_files.filename)
+			# Renaming to app.apk
+			source =  uploaded_files.filename
+			dest = 'app.apk'
+			os.rename(source, dest)
+			
+		  
+			main()
+			thesid = get_config_data('scan_id')
+			rows = getreport(thesid)
+			response = rows, {'Access-Control-Allow-Origin': '*'}  
+			update_scanid()
+			return  response
+	return jsonify(status_msg="apk not sent properly")
 
-    
+
+def getreport(scan_id):
+	sid = scan_id
+	query = "SELECT * FROM `DataDB` WHERE `ScanId` = '%s'" % sid
+	rows = select_query(query)
+	rowarray_list = []
+	for row in rows:
+		d = dict(zip(row.keys(), row))   # a dict with column names as keys
+		rowarray_list.append(d)
+	json_data = rowarray_list
+	data = json_data[0]
+
+	null_key_list = []
+	for key, value in data.items():
+		val_list = eval(value)
+		if not val_list:
+			# print(key)
+			null_key_list.append(key)
+
+	# removing all unused components
+	for key in null_key_list:
+		del data[key]
+
+	return data
+
+@app.route("/testbed")
+def test():
+	
+	return "Api testZone"
+
+
 
 
 
 @app.route('/')
 def func():
-    return "Adhrt up and flying hiGh *-*"
+	return "Adhrt up and flying hiGh *-*"
 
-def getreport(scan_id):
-   sid = scan_id
-   query = "SELECT * FROM `DataDB` WHERE `ScanId` = '%s'" % sid
-   rows = select_query(query)
-   return rows   
-    
+
+
+
 @app.route("/reset")
 def reset():
-    reset_db()
-    reset_scanid
-    return jsonify(sts_msg = "Resetted db and Scan Id!")
+	reset_db()
+	reset_scanid()
+	return jsonify(sts_msg = "Resetted db and Scan Id!")
 
-    
+	
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
 
 
 # curl -X POST -F file=@app.apk http://localhost:5000/scan
