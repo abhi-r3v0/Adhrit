2e7ae17f3e32d93138994ac65c17f39093188d72
Rahul Sani <rahulsani@Rahuls-MacBook-Pro.local>
2020-09-14 01:37:55 +0530

    Updated API's

--- bytecode_scanner.py
+++ bytecode_scanner.py
@@ -6,8 +6,9 @@ apk_name = "app.apk"
 print(Fore.YELLOW + "\n--------------------------------------------------")
 print(Fore.GREEN + "[INFO] " + Fore.BLUE + "SOURCE EXTRATION IN SMALI\n")
 
-smalicmd = '/usr/local/bin/apktool d  -f -r ' + apk_name + ' -o Bytecode'
+smalicmd = 'java -jar adhrit/tools/apktool.jar d  -f -r ' + apk_name + ' -o Bytecode'
 os.system(smalicmd)
 if os.path.isdir('Bytecode'):
     print(Fore.BLUE + "\n\t[+] " + Fore.YELLOW + "Extraction complete")
     parser()
+
