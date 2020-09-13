019eb5269d429a998026fd1508e6badd14974a3f
Rahul Sani <sani.rahul999@gmail.com>
2020-09-13 08:45:27 +0530

    Updated DBAccess with table for bytecode Scanning

--- adhrit/recons/dbaccess.py
+++ adhrit/recons/dbaccess.py
@@ -26,6 +26,16 @@ def create_datatable(conn):
 		except Error as e:
 			print(e)
 
+# Creating a table for storing data from Bytecode Scanning
+def create_bytecode_table(conn):
+	if conn is not None:
+		create_table = ''' CREATE TABLE IF NOT EXISTS DataDB(ScanId text PRIMARY KEY NOT NULL, Unsafe_Intent_Urls text, File_Access_Via_Urls text, Content_Access_Via_Urls text, Unencrypted_Socket_Communications text, Insecure_Socket_Factory text, No_Tls_Validity_Checks text, Sticky_Broadcasts text, Empty_Pending_Intents text, Dynamic/exported_Broadcast_Receivers text, Ecb_Instances text, Javascript_Enabled text, Overwritable_Cookie text, Weak_Dynamic_Invocation_Checks_On_Content_Providers text); '''
+		try:
+			c = conn.cursor()
+			c.execute(create_table)
+		except Error as e:
+			print(e)
+
 
 
 
@@ -39,7 +49,18 @@ def insert_datatable(conn, datadetails):
 			conn.commit()
 		except Error as e:
 			print(e)
+		return c.lastrowid
 
+# Inserting data into the Bytecode table
+def insert_bytecodetable(conn, datadetails):
+	if conn is not None:
+		insert_table = ''' INSERT INTO BytecodeDB(ScanId, Unsafe_Intent_Urls, File_Access_Via_Urls, Content_Access_Via_Urls, Unencrypted_Socket_Communications, Insecure_Socket_Factory, No_Tls_Validity_Checks, Sticky_Broadcasts, Empty_Pending_Intents, Dynamic/exported_Broadcast_Receivers, Ecb_Instances, Javascript_Enabled, Overwritable_Cookie, Weak_Dynamic_Invocation_Checks_On_Content_Providers) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?); '''
+		try:
+			c = conn.cursor()
+			c.execute(insert_table, datadetails)
+			conn.commit()
+		except Error as e:
+			print(e)
 		return c.lastrowid
 
 
@@ -72,14 +93,17 @@ def query_datatable(conn):
 # Reset the DataDB
 def truncate_table(conn):
 	if conn is not None:
-		truncate = ''' DELETE FROM DataDB '''
+		truncateDataDB = ''' DELETE FROM DataDB '''
+		truncateBytecodeDB = ''' DELETE FROM DataDB '''
 		try:
 			c = conn.cursor()
-			c.execute(truncate)
+			c.execute(truncateDataDB)
+			c.execute(truncateBytecodeDB)
 			conn.commit()
 		except Error as e:
 			print(e)
 			
+
 
 
 		
