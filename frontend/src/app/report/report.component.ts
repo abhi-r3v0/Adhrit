e0e3c90992de9476db478fadfe624f5addff03aa
sooraj <soorajsomans@gmail.com>
2020-09-22 12:43:44 +0530

    Adding forms validation

--- frontend/src/app/report/report.component.ts
+++ frontend/src/app/report/report.component.ts
@@ -5,8 +5,7 @@ import { KeyValuePipe } from '@angular/common';
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./plugins/fontawesome-free/css/all.min.css', './plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
-  './plugins/icheck-bootstrap/icheck-bootstrap.min.css', './plugins/jqvmap/jqvmap.min.css', './dist/css/adminlte.min.css', './plugins/overlayScrollbars/css/OverlayScrollbars.min.css', './plugins/daterangepicker/daterangepicker.css', './plugins/summernote/summernote-bs4.min.css']
+  styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
 
