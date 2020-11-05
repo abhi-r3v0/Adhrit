16bacd3489766d14924bd9d85bfaefeee2657695
sooraj <soorajsomans@gmail.com>
2020-11-05 16:28:01 +0530

    adding vulnerabilities type

--- frontend/src/app/report/report.component.ts
+++ frontend/src/app/report/report.component.ts
@@ -9,59 +9,71 @@ import { KeyValuePipe } from '@angular/common';
 })
 export class ReportComponent implements OnInit {
 
-  
-  constructor(private apkDataService:ApkDataService) { }
-  res:any
-  test:any
-  reportmanifest:any
-  reportbytecode:any
-  reportsecrets:any
-  scanType="";
+
+  constructor(private apkDataService: ApkDataService) { }
+  res: any
+  test: any
+  reportmanifest: any
+  reportbytecode: any
+  reportsecrets: any
+  scanType = "";
   ngOnInit(): void {
-    this.res=this.apkDataService.scannedData;
+    this.res = this.apkDataService.scannedData;
     console.log(this.res)
     console.log(this.res.hash_key)
     this.getManifestData()
-   // console.log(this.res[0])
-  
-/*   this.test = Object.keys(this.res).map(key => ({type: key, value: this.res[key]}))
-  console.log(this.test);
-  console.log(this.test[0]) */
-  
-  
-  }
-  
-     getManifestData(){
-       this.reportbytecode = null;
-       this.reportsecrets = null;
-       this.scanType="manifest";
-      this.apkDataService.getReportData(this.res.hash_key,this.scanType).subscribe(data => {
-        this.reportmanifest=data;
-        console.log(data)
-        console.log("Manifest Scan:"+data)
-      });
-  } 
-  getBytecodeData(){
-    this.reportmanifest = null;
-    this.reportbytecode = null;
-    this.reportsecrets = null;
-    this.scanType="bytecode";
-   this.apkDataService.getReportData(this.res.hash_key,this.scanType).subscribe(data => {
-     this.reportbytecode=data;
-     console.log(data)
-     console.log("bytecode Scan:"+data)
-   });
-}
+    // console.log(this.res[0])
 
-getSecretData(){
+    /*   this.test = Object.keys(this.res).map(key => ({type: key, value: this.res[key]}))
+      console.log(this.test);
+      console.log(this.test[0]) */
+
+
+  }
+
+  getManifestData() {
+    this.reportbytecode = null;
+    this.reportsecrets = null;
+    this.scanType = "manifest";
+    this.apkDataService.getReportData(this.res.hash_key, this.scanType).subscribe(data => {
+      this.reportmanifest = data;
+      console.log(data)
+      console.log("Manifest Scan:" + data)
+    });
+  }
+  getBytecodeData() {
     this.reportmanifest = null;
     this.reportbytecode = null;
     this.reportsecrets = null;
-  this.scanType="secrets";
- this.apkDataService.getReportData(this.res.hash_key,this.scanType).subscribe(data => {
-   this.reportsecrets=data;
-   console.log(data)
-   console.log("Manifest Scan:"+data)
- });
-} 
+    this.scanType = "bytecode";
+    this.apkDataService.getReportData(this.res.hash_key, this.scanType).subscribe(data => {
+      this.reportbytecode = data;
+      console.log(data)
+      console.log("bytecode Scan:" + data)
+    });
+  }
+
+  getSecretData() {
+    this.reportmanifest = null;
+    this.reportbytecode = null;
+    this.reportsecrets = null;
+    this.scanType = "secrets";
+    this.apkDataService.getReportData(this.res.hash_key, this.scanType).subscribe(data => {
+      this.reportsecrets = data;
+      console.log(data)
+      console.log("Manifest Scan:" + data)
+    });
+  }
+
+  getVulnerabilitiesData() { 
+  this.reportmanifest = null;
+  this.reportbytecode = null;
+  this.reportsecrets = null;
+  this.scanType = "vulnerabilities";
+  this.apkDataService.getReportData(this.res.hash_key, this.scanType).subscribe(data => {
+  this.reportsecrets = data;
+  console.log(data)
+  console.log("Manifest Scan:" + data)
+});
+  }
 }
